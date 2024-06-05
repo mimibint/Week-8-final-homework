b29a9bcf0d4f779622a21181eb04ea98f78ab410
mimibint <mbm.maryam@gmail.com>
2024-06-05 21:12:29 +0100

    edited api key

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "f40bdf4afd6a305e4ac536bo92271dt8";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
